505f38843c42f1c03a7eb17fdd6bd5fb3e533778
Luis Schweigard <luis.schweigard@gmail.com>
2024-09-05 16:26:17 +0200

    Add home packages

--- home.nix
+++ home.nix
@@ -3,9 +3,24 @@
   home.username = lib.mkForce "luis.schweigard";
   home.stateVersion = "23.05"; # Required for some reason
 
-  programs.home-manager.enable = true;
+  programs = {
+    home-manager.enable = true;
+  };
 
-  home.packages = [
-    pkgs.htop
+  # For zsh
+  #     autosuggestion.enable = true;
+  #     syntaxHighlighting.enable = true;
+
+  home.packages = with pkgs; [
+    htop
+    jq
+    yq
+    sops
+    kubernetes-helm
+    kubectl
+    kubectx
+    fd
+    k9s
+    pre-commit
   ];
 }
